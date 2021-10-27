476b50d41f21b2e1bf41698e022ac2ae8ef26798
brotherson67 <mitchell.brotherson@gmail.com>
2021-10-26 23:07:40 -0600

    issue-9 added map, need to organize HTML to make bigger

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
-const areaSearched = document.getElementById("areaSearched").value;
-console.dir(areaSearched)
-console.log(areaSearched)
 
-const callMap = (areaSearched) => {
-    const callURL = ""
-}
+mapboxgl.accessToken = 'pk.eyJ1IjoieW91cmJyb3RoZXJzb24iLCJhIjoiY2t2N29tOHc4MXI5ZDJvcDY0Z3BpOGdscSJ9.8CudkVmUxTWSc71lYIYdag';
+
+    const map = new mapboxgl.Map({
+    container: 'mapBox', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    //make function to find latitude and longitude from given input
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+    });
+
+console.dir(map)
