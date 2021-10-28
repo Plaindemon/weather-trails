e5fcd95ef19780505672b131fbd31ab2fee7c7be
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-10-28 16:40:44 -0600

    added var to connect the search box input to the click on search btn not finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,63 @@
 // Global Variables
 // var to connect to input in search 
-var inputForm = document.getElementById('input-city');
-var searchCity = document.getElementById('search-city-btn');
-
-var formData = function() {
-    inputForm.addEventListener('click', function(){
-        console.log(searchCity);
-    });
-}
-var searchBtn = function() {
-    // reacts to click on the search button
-    searchCity.addEventListener('click', function(){
-        alert("search button was clicked");
-        
-    });
-    console.log("HELLO");
-};
-
-// searchBtn();
+var inputForm = document.querySelector("#input-city");
+var searchCityBtn = document.querySelector("#search-city-btn");
 
 // code using nps.gov API - use to get info from about places near the location desired
 // $dataURL = 'https://developer.nps.gov/api/v1/parks?stateCode=me';
+// console.log(inputForm);
+// console.log(searchCityBtn);
 
-var places = fetch("https://developer.nps.gov/api/v1/parks?limit=15&start=10&fields=fullName%2C%20%20URL%2C%20state%2C%20%20description%2C%20activities%2C%20directionsInfo%2C%20directionsURL%2C%20weatherInfo%2C%20", {
+var submitSearch = function(event) {
+    event.preventDefault();
+    console.log(event);
+};
+var clickSearch = function(event) {
+    var searchActivity = event.target.getElementById("#input-city-form");
+    console.log(searchActivity);
+}
+
+var parks = fetch("https://developer.nps.gov/api/v1/parks?limit=15&start=10&fields=fullName%2C%20%20URL%2C%20state%2C%20%20description%2C%20activities%2C%20directionsInfo%2C%20directionsURL%2C%20weatherInfo%2C%20", {
 	"method": "GET",
 	"headers": {
 		"x-api-key": "4fxi1Pok4kgde26ywgQnyEaaxknpMlmKw3svW5lP",
 	}
-})
-.then(response => {
+    })
+    .then(response => {
     // log an array to the console using data function
     response.json().then(function(data) {
         console.log(data);
       });
-	// console.log(response);
-    console.log("inside", places);
-})
-.catch(err => {
-	console.error(err);
-});
+	    // console.log(response);
+        console.log("inside", parks);
+    })
+    .catch(err => {
+	    console.error(err);
+    });
 
-console.log("outside", places);
 
+console.log("outside", parks);
+
+//event Listener 
+inputForm.addEventListener("submit", submitSearch);
+// searchCityBtn.addEventListener("click", )
+
+
+// var formData = function() {
+//     inputForm.addEventListener('click', function(){
+//         console.log(searchCity);
+//     });
+// }
+// var searchBtn = function() {
+//     // reacts to click on the search button
+//     searchCity.addEventListener('click', function(){
+//         alert("search button was clicked");
+        
+//     });
+//     console.log("HELLO");
+// };
+
+// // searchBtn();
 
 
 
@@ -49,12 +65,12 @@ console.log("outside", places);
 mapboxgl.accessToken = 'pk.eyJ1IjoieW91cmJyb3RoZXJzb24iLCJhIjoiY2t2N29tOHc4MXI5ZDJvcDY0Z3BpOGdscSJ9.8CudkVmUxTWSc71lYIYdag';
 
 const map = new mapboxgl.Map({
-container: 'mapBox', // container ID
-style: 'mapbox://styles/mapbox/streets-v11', // style URL
-//make function to find latitude and longitude from given input
-center: [-74.5, 40], // starting position [lng, lat]
-//data points array will be added right here which will call the trails
-zoom: 9 // starting zoom
+    container: 'mapBox', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    //make function to find latitude and longitude from given input
+    center: [-74.5, 40], // starting position [lng, lat]
+    //data points array will be added right here which will call the trails
+    zoom: 9 // starting zoom
 });
 console.dir(map)
 // 5 day weather forecast
