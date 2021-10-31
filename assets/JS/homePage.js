37fa16e5dbe63da578d54c664741aef8e9331ec1
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-10-30 22:13:52 -0600

    added a weatherInfo clickable tab and a directions info tab

--- assets/JS/homePage.js
+++ assets/JS/homePage.js
@@ -1,9 +1,15 @@
 // Global Variables
 // var to connect to input in search 
 var inputSearchName = document.querySelector("#input-state");
+var activitySearchBtn = document.querySelector("#activities-btn");
+var directionsInfoBtn = document.querySelector('#directions-btn')
+var weatherInfoBtn = document.querySelector('#weather-info-btn')
 var searchCityBtn = document.querySelector("#search-city-btn");
 var inputContainerEl = document.querySelector("#input-container");
 var parks = [];
+var weatherInfo = [];
+var directionsInfo = [];
+var directionsUrl
 
 // code using nps.gov API - use to get info from about places near the location desired 
 var requestOptions = {
@@ -13,7 +19,7 @@ var requestOptions = {
 const apiKeyNps = "4fxi1Pok4kgde26ywgQnyEaaxknpMlmKw3svW5lP";
 
 function parkInfo() {
-
+    // fetch a response from nps 
     fetch("https://developer.nps.gov/api/v1/parks?stateCode=" + inputSearchName.value + "&api_key=" + apiKeyNps + "&fields=description,fullName,url,activities,latitude,longitude,directionsInfo,addresses,weatherInfo,name,directionsUrl,operatingHours", requestOptions)
     .then(response => response.json())
     .then(result => { 
@@ -27,6 +33,7 @@ function parkInfo() {
             var aEl = document.createElement('a');
             aEl.setAttribute('href',result.data[i].url);
             aEl.innerText = result.data[i].fullName;
+            
             liEl.appendChild(aEl);
             ulEl.appendChild(liEl);
         }
@@ -34,9 +41,59 @@ function parkInfo() {
         inputContainerEl.appendChild( ulEl);
     })
     .catch(error => console.log('error', error));
+
 }
 
+function parkDirectionsInfo() {
+    fetch("https://developer.nps.gov/api/v1/parks?stateCode=" + inputSearchName.value + "&api_key=" + apiKeyNps + "&fields=description,fullName,url,activities,latitude,longitude,directionsInfo,addresses,weatherInfo,name,directionsUrl,operatingHours", requestOptions)
+    .then(response => response.json())
+    .then(result => { 
+       console.log(result.data[0]);
+        directionsInfo = result.data;
+        var ulEl = document.createElement("ul");
+        // do all the html stuff to display the parks
+        for(var i = 0; i < result.data.length; i++) {
+            
+            var liEl = document.createElement("li");
+            var aEl = document.createElement('a');
+            aEl.setAttribute('href',result.data[i].url);
+            aEl.innerText = result.data[i].directionsInfo;
+            liEl.appendChild(aEl);
+            ulEl.appendChild(liEl);
+        }
+        inputContainerEl.innerText = '';
+        inputContainerEl.appendChild( ulEl);
+    })
+    .catch(error => console.log('error', error));
 
+
+}
+
+function parkWeatherInfo() {
+    fetch("https://developer.nps.gov/api/v1/parks?stateCode=" + inputSearchName.value + "&api_key=" + apiKeyNps + "&fields=description,fullName,url,activities,latitude,longitude,directionsInfo,addresses,weatherInfo,name,directionsUrl,operatingHours", requestOptions)
+    .then(response => response.json())
+    .then(result => { 
+       console.log(result.data[0]);
+        weatherInfo = result.data;
+        var ulEl = document.createElement("ul");
+        // do all the html stuff to display the parks
+        for(var i = 0; i < result.data.length; i++) {
+            
+            var liEl = document.createElement("li");
+            var aEl = document.createElement('a');
+            aEl.setAttribute('href',result.data[i].url);
+            aEl.innerText = result.data[i].weatherInfo;
+            liEl.appendChild(aEl);
+            ulEl.appendChild(liEl);
+        }
+        inputContainerEl.innerText = '';
+        inputContainerEl.appendChild( ulEl);
+    })
+    .catch(error => console.log('error', error));
+
+
+
+}
 
 
 var formData = function() {
@@ -51,7 +108,17 @@ searchCityBtn.addEventListener('click', function(){
         // alert("search button was clicked");
         parkInfo();
         
+        
 });
+
+directionsInfoBtn.addEventListener('click', function(){
+    parkDirectionsInfo();
+})
+weatherInfoBtn.addEventListener('click', function(){
+    console.log(parkWeatherInfo());
+    parkWeatherInfo();
+})
+
 
 
 
